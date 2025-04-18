e6bbc76d5d6eaff4a167b9362b9b1d542f359dab
Ahmed Abdulrahim <medo1562003@gmail.com>
2025-04-18 04:23:02 +0200

    Edited compile.bat

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 @echo off
 rem Configure these variables to match your setup
 rem MiKTeX installation path
-set MIKTEX_PATH=C:\Users\ibrahim\AppData\Local\Programs\MiKTeX\miktex\bin\x64
+set MIKTEX_PATH=C:\Users\MaStEr\AppData\Local\Programs\MiKTeX\miktex\bin\x64
 rem Your name (will be used in the output PDF filename)
-set USER_NAME=Ibrahim.Ahmed.Net
+set USER_NAME=MASTER
 
 rem Configure input and output filenames
 set INPUT_FILE=resume.tex
@@ -17,19 +17,11 @@ if not exist "logs" mkdir logs
 
 echo Starting LaTeX compilation process...
 
-echo Running first xelatex pass...
-xelatex -quiet -aux-directory=logs -output-directory=logs %INPUT_FILE%
+echo Running first pdflatex pass...
+pdflatex -quiet -aux-directory=logs -output-directory=logs %INPUT_FILE%
 
-echo Running biber...
-cd logs
-biber resume
-cd ..
-
-echo Running second xelatex pass...
-xelatex -quiet -aux-directory=logs -output-directory=logs %INPUT_FILE%
-
-echo Running final xelatex pass...
-xelatex -quiet -aux-directory=logs -output-directory=logs %INPUT_FILE%
+echo Running second pdflatex pass...
+pdflatex -quiet -aux-directory=logs -output-directory=logs %INPUT_FILE%
 
 echo Moving PDF to main directory...
 move /Y "logs\resume.pdf" "%OUTPUT_FILE%"
